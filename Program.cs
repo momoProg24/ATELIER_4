a3843ffe583ae485c4026b35041c99b1ff7abd9e
elisalac <elisa.lacombe@icloud.com>
2024-03-22 13:33:25 -0400

    c

--- Program.cs
+++ Program.cs
@@ -11,25 +11,16 @@ using System.Transactions;
 
 static void TestFabriques()
 {
-    
+
     var p = new FabriqueBoites().Créer("mono J'aime mon \"prof\"");
     Console.WriteLine(new Boite(p));
 
-   
-     p = new FabriqueBoites().Créer("cv\nmono J'aime mon \"prof\"\nmono moi itou");
-    Console.WriteLine(new Boite(p));
-    
 
-    
-   //p = new FabriqueBoites().Créer("ch\nmono J'aime mon \"prof\"\nmono moi itou"); // MARCHE PAS
-   //Console.WriteLine(new Boite(p));
-   
-    /*
-   p = new FabriqueBoites().Créer(
-      "ch\ncv\nmono J'aime mon \"prof\"\nmono moi itou\nmono eh ben");  // MARCHE PAS 
-   Console.WriteLine(new Boite(p));
-    */
-   
+    p = new FabriqueBoites().Créer("cv\nmono J'aime mon \"prof\"\nmono moi itou");
+    Console.WriteLine(new Boite(p));
+
+    p = new FabriqueBoites().Créer("ch\nmono J'aime mon \"prof\"\nmono moi itou");
+    Console.WriteLine(new Boite(p));
 }
 
 TestFabriques();
